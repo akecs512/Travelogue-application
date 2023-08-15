1b3ae47c4e4c48dbf83f451ace6fa53d93a4896c
alexc <alexcolwell13@gmail.com>
2023-08-15 14:36:03 -0500

    fixed comments and errors

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const routes = require('./controllers2');
 const exphbs = require('express-handlebars');
 const path = require("path");
 const helpers = require('./utils/helpers');
-// const styles = require(path.join(__dirname, 'public/assets/css'));
-// const fs = require("fs");
+const fs = require("fs");
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
