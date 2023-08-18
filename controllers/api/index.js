df29e6c86069560e9f0775b013493fd83028dd3a
Marialdinha <marialda@bellsouth.net>
2023-08-17 22:00:47 -0500

    removed start-date and end-date from travel-info.js - now just date

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const travelRoutes = require('./travel_info_routes');
+const userRoutes = require('./user_routes');
+const pageRoutes = require('./pages_route');
+
+router.use('/travel', travelRoutes);
+router.use('/user', userRoutes);
+router.use('/pages', pageRoutes);
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const User  = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/travelogue', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('travelogue');
+});
+
+router.get('/dashboard', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('dashboard');
+});
+
+module.exports = router;
